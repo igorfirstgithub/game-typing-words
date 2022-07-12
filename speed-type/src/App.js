5f1e974456a671dc6a5f371e912eda655391b937
Igor <bigbookreader@gmail.com>
2022-07-12 14:06:53 +0300

    Cycled gaming with words counting

--- speed-type/src/App.js
+++ speed-type/src/App.js
@@ -1,58 +1,51 @@
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react";
 
 function App() {
-    
   const [text, setText] = useState("");
   const [timeRemaining, setTimeRemaining] = useState(5);
   const [isGameRunning, setIsGameRunning] = useState(false);
-  
+  const [numWords, setNumWords] = useState(0);
 
-  /*useEffect(() => {
-    const timeoutID = setTimeout(() => setTimeRemained(prevTime => prevTime - 1), 1000);
-    if (timeRemained === 0) {
-      clearTimeout(timeoutID)
-    }
-  }, [timeRemained]);
-  */
-  
   useEffect(() => {
     if (isGameRunning === true && timeRemaining > 0) {
-      setTimeout(() => setTimeRemaining(prevTime => prevTime - 1), 1000)
-     
+      setTimeout(() => setTimeRemaining((prevTime) => prevTime - 1), 1000);
     } else if (timeRemaining === 0) {
-      setIsGameRunning(false)
+      setIsGameRunning(false);
+      setNumWords(countWords(text));
     }
-    console.log(isGameRunning);
-  }
-    ,[timeRemaining, isGameRunning])
-    
+    //    console.log(isGameRunning);
+  }, [timeRemaining, isGameRunning]);
 
-    function changeText(event) {
-        setText(event.target.value);
-    }
-  
-  
-  
+  function changeText(event) {
+    setText(event.target.value);
+  }
+
   function countWords(text) {
-    const wordsArray = text.trim().split(" ").filter(el => el !== "");
-    console.log(wordsArray.length);
+    const wordsArray = text
+      .trim()
+      .split(" ")
+      .filter((el) => el !== "");
+    return wordsArray.length;
   }
 
   function startGame() {
-    setIsGameRunning(true)
+    if (isGameRunning === false) {
+      setIsGameRunning(true);
+      setNumWords(0);
+      setTimeRemaining(5);
+      setText("");
+    }
   }
-    
-    //console.log(text);
-    
-    return (
-        <div>
-            <h1>How fast do you type?</h1>
-            <textarea value={text} name="textarea" onChange={changeText} />
-            <h4>Time remainng: {timeRemaining}</h4>
-            <button onClick={startGame}>Start</button>
-        <h1>Word count: ???</h1>
-        </div>
-    )
+
+  return (
+    <div>
+      <h1>How fast do you type?</h1>
+      <textarea value={text} name="textarea" onChange={changeText} />
+      <h4>Time remainng: {timeRemaining}</h4>
+      <button onClick={startGame}>Start</button>
+      <h1>Word count: {numWords}</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
