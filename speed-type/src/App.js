d3dd5ccd389396640538ef49a8a3d578d8c34a86
Igor <bigbookreader@gmail.com>
2022-07-12 11:46:37 +0300

    Counting words in textarea

--- speed-type/src/App.js
+++ speed-type/src/App.js
@@ -7,6 +7,11 @@ function App() {
     function changeText(event) {
         setText(event.target.value);
     }
+  
+  function countWords(text) {
+    const wordsArray = text.trim().split(" ").filter(el => el != "");
+    console.log(wordsArray.length);
+  }
     
     //console.log(text);
     
@@ -15,7 +20,7 @@ function App() {
             <h1>How fast do you type?</h1>
             <textarea value={text} name="textarea" onChange={changeText} />
             <h4>Time reminaing: ???</h4>
-            <button>Start</button>
+            <button onClick={() => countWords(text)}>Start</button>
         <h1>Word count: ???</h1>
         </div>
     )
